4a7eda98c2ff3095fde9e852c1c9562a94e25059
jasonmosley21 <jdm449@drexel.edu>
2022-05-26 18:38:57 -0400

    class 5/26
    tried to fix nav, started the join the wonder section

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,26 +1,44 @@
-
-   
 let burgerBtn = document.querySelector("#burger-btn");
 let mainMenu = document.querySelector("#main-menu");
 
+let closeBtn = document.querySelector("#close-btn");
+
 let canSeeMenu = false;
 
-burgerBtn.addEventListener("click", () => {
-    
+// console.log("this is can see" + canSeeMenu)
 
-    if(!canSeeMenu){
+burgerBtn.addEventListener("click", () => {
+    console.log("this is can see" + canSeeMenu)
+
+    if (!canSeeMenu) {
         mainMenu.classList.add("show-menu");
+        // hide the burger
+        burgerBtn.classList.add("show-hide");
+
+        // show the X
+        closeBtn.classList.remove("show-hide");
+
+
         canSeeMenu = true;
-    }else{
+        console.log("show")
+
+
+    } else {
         mainMenu.classList.remove("show-menu");
+        // hide the burger
+        burgerBtn.classList.remove("show-hide");
+
+        // hide the X
+        closeBtn.classList.add("show-hide");
         canSeeMenu = false;
+        console.log("hide")
     }
 })
 
 
-let closeBtn = document.querySelector("#close-btn");
+
 
 closeBtn.addEventListener("click", () => {
-        mainMenu.classList.remove("show-menu");
-        canSeeMenu = false;
+    mainMenu.classList.remove("show-menu");
+    canSeeMenu = false;
 })
